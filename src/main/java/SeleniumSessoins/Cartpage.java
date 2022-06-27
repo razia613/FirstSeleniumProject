89c4f88112dbf969b889436f14ea32ba7dea1ca8
unknown <razia613@gmail.com>
2022-06-27 13:58:59 +0530

    modified cart page

--- src/main/java/SeleniumSessoins/Cartpage.java
+++ src/main/java/SeleniumSessoins/Cartpage.java
@@ -8,6 +8,8 @@ public class Cartpage {
 	{
 		cart_page = By.id("cart_page");
 		System.out.println("This is the Cart Page");
+		System.out.println("Pushed the code to Main remote branch");
+		
 	}
 
 }
