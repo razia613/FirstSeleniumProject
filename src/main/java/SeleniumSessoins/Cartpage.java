d898f3cfc82e84a2204715e316bf1e5a4a2e00fb
unknown <razia613@gmail.com>
2022-06-27 12:34:16 +0530

    added By loctors in the cart page

--- src/main/java/SeleniumSessoins/Cartpage.java
+++ src/main/java/SeleniumSessoins/Cartpage.java
@@ -1,5 +1,13 @@
 package SeleniumSessoins;
 
+import org.openqa.selenium.By;
+
 public class Cartpage {
+	private By cart_page;
+	public void printCart()
+	{
+		cart_page = By.id("cart_page");
+		System.out.println("This is the Cart Page");
+	}
 
 }
